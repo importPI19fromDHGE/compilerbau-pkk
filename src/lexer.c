8f4ed022eda75ffae0e1dd0f08676ac69add6de4
Felix Prillwitz <36958734+photovoltex@users.noreply.github.com>
2022-03-28 12:36:29 +0200

    only recognise token type

--- src/lexer.c
+++ src/lexer.c
@@ -7,6 +7,7 @@
 #include <malloc.h>
 #include <string.h>
 
+#include "debug.h"
 #include "lexer.h"
 
 // global `char *input_buf` is the input source code
@@ -35,12 +36,6 @@ word_t *words;
 int word_count = 0;
 
 int line_nr = 1, col_nr = 1;
-
-void recognise_token_type(const char *token) {
-    // todo: is this is a number? --> oper_const
-    // todo: is this in the nametab? --> add if it isn't
-    //
-}
 
 bool is_delimiter(char c) {
     bool is_delimiter = false;
@@ -86,21 +81,93 @@ int read_word(const int start_pos) {// start_pos ist globales Offset zu input_bu
     // fill struct
     word_t word = {
         input_buf + start_pos,
-        (word_len > 0) ? word_len + 1 : word_len,
+        word_len,
         col_nr,
         line_nr
     };
 
     col_nr += word_len;
 
-    // write to words array
-    words[word_count] = word;
-    printf("[ln: %d, col: %d] %.*s\n", word.line_nr, word.col_nr, word_len, word.word_ptr);
-
-    word_count++;
-
+    // write to words array and increment word_count for next iteration
+    words[word_count++] = word;
     return start_pos + word_len;
 }
+
+type_t recognise_token_type(const char *word) {
+    type_t type;
+
+    // set matching type if the first char is a delimiter
+    switch (word[0]) {
+        case '(': type = oper_lpar;
+            break;
+        case ')': type = oper_rpar;
+            break;
+        case ',': type = oper_sep;
+            break;
+        case '|': type = oper_abs;
+            break;
+        case '^': type = oper_pow;
+            break;
+        case '*': type = oper_mul;
+            break;
+        case '/': type = oper_div;
+            break;
+        case '+': type = oper_add;
+            break;
+        case '-': type = oper_sub; // als Token sind unäre (-) auch oper_sub, siehe turtle-types.h
+            break;
+        case '=': type = oper_equ;
+            break;
+        case '<': //type = oper_less; // kann oper_less oder oper_nequ oder oper_lequ sein
+            switch (word[1]) {
+                case '=': type = oper_lequ;
+                    break;
+                case '>': type = oper_nequ;
+                    break;
+                default: type = oper_less;
+                    break;
+            }
+            break;
+        case '>': // kann oper_grtr oder oper_gequ sein
+            type = (input_buf[1] == '=') ? oper_gequ : oper_grtr;
+            break;
+        default: // word is from type name_any or oper_const
+            type = oper_const;
+            // check if oper_const contains any alphabetic char
+            //  E/e is ignored due to task specifications
+            for (int i = 0; word[i] != '\0'; i++) {
+                if ((word[i] >= 'a' && word[i] <= 'z')
+                    || (word[i] >= 'A' && word[i] <= 'Z')) {
+                    // drop out if an alphabetic char was found
+                    type = name_any;
+                    break;
+                }
+            }
+            break;
+    }
+
+    // type already found
+    if (type != name_any) {
+        return type;
+    }
+
+    // search a matching name in name_tab
+    for (int i = 0; i < name_tab_length; i++) { // lineare search through name_tab
+        if (strcmp(word, name_tab[i].name) == 0) { // word matches
+            type = name_tab[i].type;
+            break;
+        }
+    }
+
+    // type in name_tab found
+    if (type != name_any) {
+        return type;
+    }
+
+    name_tab[name_tab_length++] = (nameentry_t) {type, word, {} };
+    return type;
+}
+
 
 void lex(void) {
     insertArray(&token_stream, (token_t) {tok_bofeof, -1});
@@ -113,6 +180,16 @@ void lex(void) {
     }
     // ab hier gesamter Input getrennt
 
+    for (int i = 0; i < word_count; i++) {
+        char word[words[i].length + 1];
+        memset(word, '\0', sizeof(word));
+        strncpy(word, words[i].word_ptr, words[i].length);
+        type_t token_type =  recognise_token_type(word);
+
+        printf("[ln: %d, col: %d] \"%.*s\" of type [%s]\n",
+               words[i].line_nr, words[i].col_nr, words[i].length, words[i].word_ptr,
+               DEBUG_TURTLE_TYPE_NAMES[token_type]);
+    }
 
 
     insertArray(&token_stream, (token_t) {tok_bofeof, -1});
--- src/lexer.h
+++ src/lexer.h
@@ -28,28 +28,33 @@ typedef struct {
 extern int word_count;
 extern word_t *words;
 
-/// Reads a word and writes information to `words`
-/// \param start_pos globales Offset zu input_buf
-/// \return position for the next `read_word` call
-int read_word(int start_pos);
-
-/// Determines the type of a token.
-/// \param token
-void recognise_token_type(const char *token);
-
-/// Performs a linear search over the nameentry_t `name_tab`, looking for a given token
-/// \param token a \0-terminated String, the token to search for
-/// \return The index of the token in the `nametab`; -1 if not found
-int find_in_nametab(const char *token);
-
-/// Insert a new name into the nametab
-/// \param nameentry
-/// \return the nametab-index of the new entry
-int insert_in_nametab(nameentry_t nameentry);
 
 /// Performs a linear search over the delimiters-Array
 /// \param c The character to check
 /// \return True if c is a delimiter, False otherwise
 bool is_delimiter(char c);
+
+/// Reads a word and writes information to `words`
+/// \param start_pos globales Offset zu input_buf
+/// \returns position for the next `read_word` call
+int read_word(int start_pos);
+
+
+/// Performs a linear search over the nameentry_t `name_tab`, looking for a given token
+/// \param word a \0-terminated String, the token to search for
+/// \return The index of the token in the `nametab`; -1 if not found
+int find_in_name_tab(const char *word);
+
+/// Insert a new name into the nametab
+/// \param nameentry
+/// \return the nametab-index of the new entry
+int insert_in_name_tab(nameentry_t nameentry);
+
+/// Determines the type of a given word.
+/// \param word for which the token type should be determined.
+///             Has to be a valid c string.
+/// \returns token type of given word.
+///           if name_any is returned the index of name_tab is the last name_tab_length - 1.
+type_t recognise_token_type(const char *word);
 
 #endif //COMPILERBAU_PKK_LEXER_H
